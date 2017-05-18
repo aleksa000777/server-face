14b8ce7c6e983c5bd9ef4fa949634ed15a8eb6ab
Aleksandra Matiev <aleksa000777@mail.ru>
2017-05-18 09:56:16 -0400

    set .env

---
+++ README.md
@@ -0,0 +1,1 @@
+***https://calm-savannah-34510.herokuapp.com/***
--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 var express = require('express')
 var app = express()
+require('dotenv').config()
 
 var Clarifai = require('clarifai');
 
 // instantiate a new Clarifai app passing in your clientId and clientSecret
 var myApp = new Clarifai.App(
-  'tSmyrPRAUvBVfbqdqMG2SLTIWW11S2QBdzUDZQFZ',
-  'WgfiHBkMmBoooQj7vYIfrWEcrFL6YfOjWcHxvWA3'
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET
 );
 
 
 
 
 app.get('*', function (req, res) {
-  let param = req.params["0"].substring(1)
+  let param = req.params["0"]
   let result
-  myApp.models.predict("e466caa0619f444ab97497640cefc4dc", ''+req.params["0"].substring(1)+'').then(
-    function(response) {
-      console.log('success back end!!');
-      console.log(response.outputs[0].data.regions[0].data.face.identity.concepts[0]);
-      result = response.outputs[0].data.regions[0].data.face.identity.concepts[0];
-      console.log(result);
-      res.send(JSON.stringify({ result }))
-    },
-    function(err) {
-      console.error(err);
-    }
-  )
+  if (param == '/'){
+    res.send("Server running")
+  }
+  else{
+    myApp.models.predict("e466caa0619f444ab97497640cefc4dc", ''+param.substring(1)+'').then(
+      function(response) {
+        console.log('success back end!!');
+        console.log(response.outputs[0].data.regions[0].data.face.identity.concepts[0]);
+        result = response.outputs[0].data.regions[0].data.face.identity.concepts[0];
+        console.log(result);
+        res.send(JSON.stringify({ result }))
+      },
+      function(err) {
+        console.error(err);
+      }
+    )
+  }
+
 })
 
 
