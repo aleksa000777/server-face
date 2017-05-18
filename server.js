ec8d9f7a4687ad1d6a4804e13455a721f1378853
Aleksandra Matiev <aleksa000777@mail.ru>
2017-05-18 11:02:46 -0400

    standart js

--- server.js
+++ server.js
@@ -2,43 +2,36 @@ var express = require('express')
 var app = express()
 require('dotenv').config()
 
-var port =  process.env.PORT || 3000
+var port = process.env.PORT || 3000
 
-var Clarifai = require('clarifai');
+var Clarifai = require('clarifai')
 
 // instantiate a new Clarifai app passing in your clientId and clientSecret
 var myApp = new Clarifai.App(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET
-);
-
+)
 
 app.get('*', function (req, res) {
-  let param = req.params["0"]
+  let param = req.params['0']
   let result
-  console.log('here');
-  if (param == '/'){
-    res.send("Server running")
-  }
-  else{
-    myApp.models.predict("e466caa0619f444ab97497640cefc4dc", ''+param.substring(1)+'').then(
-      function(response) {
-        console.log('success back end!!');
-        console.log(response.outputs[0].data.regions[0].data.face.identity.concepts[0]);
-        result = response.outputs[0].data.regions[0].data.face.identity.concepts[0];
-        console.log(result);
+  if (param === '/') {
+    res.send('Server running')
+  } else {
+    myApp.models.predict('e466caa0619f444ab97497640cefc4dc', '' + param.substring(1) + '').then(
+      function (response) {
+        console.log('success back end!!')
+        result = response.outputs[0].data.regions[0].data.face.identity.concepts[0]
+        console.log(result)
         res.send(JSON.stringify({ result }))
       },
-      function(err) {
-        console.error(err);
+      function (err) {
+        console.error(err)
       }
     )
   }
-
 })
 
-
-
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port: ' + port)
 })
