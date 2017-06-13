54a42ca13ee394466689247f2f2411719f8d0f4c
Aleksandra Matiyev <aleksa000777@mail.ru>
2017-06-13 17:21:39 -0400

    working server

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ***https://calm-savannah-34510.herokuapp.com/***
+express-formidable - change middleware.js with `changeMiddleware.js`
---
+++ changeMiddleware.js
@@ -0,0 +1,30 @@
+// npm package express-formidable
+// change middleware.js
+'use strict';
+
+const formidable = require('formidable');
+
+function parse(opts) {
+  return (req, res, next) => {
+    const form = new formidable.IncomingForm();
+    Object.assign(form, opts);
+
+    form.on('error', (err) => {
+      next(err);
+      return;
+    });
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      Object.assign(req, { fields, files });
+      next();
+    });
+  };
+}
+
+module.exports = parse;
+exports.parse = parse; // backword compatibility
